e47e466fa98089bc92d8c944fa887c29a98ec06d
LimTae-git <lth9122@naver.com>
2019-11-08 18:34:52 +0900

    crypto update

--- study-signup/routes/user.js
+++ study-signup/routes/user.js
@@ -1,26 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var models = require("../models");
-
+var crypto = require('crypto')
 /* GET users listing. */
 router.get('/sign_up', function(req, res, next) {
   res.render('user/signup');
 });
 
-router.post("/sign_up", function(req, res, next){
+router.post("/sign_up", async function(req, res, next){
   let body = req.body;
 
-  models.user.create({
+  let inputPassword = body.password;
+  let salt = Math.round((new Date().valueOf() * Math.random())) + "";
+  let hashPassword = crypto.createHash("sha512").update(inputPassword + salt).digest("hex");
+
+  let result = models.user.create({
     name: body.userName,
     email: body.userEmail,
-    password: body.password
+    password: body.password,
+    salt : salt
   })
-  .then( result => {
-    res.redirect("/users/sign_up");
-  })
-  .catch( err => {
-    console.log(err)
-  })
+  res.redirect("/users/sign_up");
 })
 
 module.exports = router;
